04852fcdb3e8e42efbbf00e88c12e4c7d76bf582
Retro <44505837+dankmolot@users.noreply.github.com>
2021-06-12 14:56:33 +0300

    add some steamworks api

--- src/main.cpp
+++ src/main.cpp
@@ -9,26 +9,57 @@ using namespace GarrysMod::Lua;
 using namespace std;
 
 namespace Steamworks {
+	struct CSteamworks;
 	CSteamGameServerAPIContext* GameServerAPI;
+	CSteamworks* SteamworksAPI = nullptr;
+
+	struct CSteamworks {
+		STEAM_GAMESERVER_CALLBACK(CSteamworks, OnItemDownloaded, DownloadItemResult_t);
+		STEAM_CALLBACK(CSteamworks, OnItemDownloaded2, DownloadItemResult_t);
+	};
+
+	void CSteamworks::OnItemDownloaded(DownloadItemResult_t* res)
+	{
+		cout << "test 1" << endl;
+		cout << "AppID:  " << res->m_unAppID << endl;
+		cout << "FileID: " << res->m_nPublishedFileId << endl;
+		cout << "Result: " << res->m_eResult << endl;
+	}
+
+	void CSteamworks::OnItemDownloaded2(DownloadItemResult_t* res)
+	{
+		cout << "test 2" << endl;
+		cout << "AppID:  " << res->m_unAppID << endl;
+		cout << "FileID: " << res->m_nPublishedFileId << endl;
+		cout << "Result: " << res->m_eResult << endl;
+	}
 
 	int DownloadUGC(ILuaBase* LUA)
 	{
-		Threading::Thread::Create(LUA->GetState(), [](lua_State* L, Threading::Thread* t) {
-			cout << "Thread start" << endl;
+		ISteamUGC* ugc = GameServerAPI->SteamUGC();
+		ISteamUGC* ugc2 = SteamGameServerUGC();
+		cout << ugc << ' ' << boolalpha << (ugc == nullptr) << endl;
+		cout << ugc2 << ' ' << boolalpha << (ugc2 == nullptr) << endl;
 
-			t->Sync();
-			cout << "This message is synced!" << endl;
-			t->Desync();
+		if (ugc != nullptr) {
+			cout << "ugc1" << endl;
+			ugc->DownloadItem(2509905428, false);
+		}
+			
 
-			cout << "Now i'm desynced!" << endl;
-		});
+		if (ugc2 != nullptr) {
+			cout << "ugc2" << endl;
+			ugc2->DownloadItem(2507359662, false);
+		}
 
-		return 1;
+
+		return 0;
 	}
 
 	void Initialize(ILuaBase* LUA)
 	{
-		CSteamGameServerAPIContext* api = InterfacePointers::SteamGameServerAPIContext();
+		GameServerAPI = InterfacePointers::SteamGameServerAPIContext();
+		SteamworksAPI = new CSteamworks;
 
 		Threading::Core::Initialize(LUA);
 
