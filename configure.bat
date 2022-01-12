1abc3b0fc11eba939d8d041353f3b6f04463deba
Retro <44505837+dankmolot@users.noreply.github.com>
2022-01-12 16:07:30 +0200

    some updates

--- configure.bat
+++
@@ -1,4 +0,0 @@
-"bin/premake5.exe" vs2019 %*
-
-:: Pause for windows folks
-pause
--- premake5.lua
+++ premake5.lua
@@ -1,16 +1,17 @@
 PROJECT_GENERATOR_VERSION = 3
 
 newoption({
-	trigger = "gmcommon",
-	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
-	value = "path to garrysmod_common directory"
+    trigger = "gmcommon",
+    description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
+    value = "path to garrysmod_common directory"
 })
 local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
-	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
+    "you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
 include(gmcommon)
 
 include "third-party/steam_api" -- Replaces IncludeSteamAPI
 include "third-party/tinydir"
+include "third-party/LuaThreading"
 
 CreateWorkspace({ name = "steamworks", abi_compatible = false })
     CreateProject({ serverside = true, manual_files = true })
@@ -21,6 +22,7 @@ CreateWorkspace({ name = "steamworks", abi_compatible = false })
         IncludeSDKTier0()
 
         IncludeTinydir()
+        IncludeLuaThreading()
 
         files {"src/*.cpp", "src/*.hpp"}
         sysincludedirs { "third-party/tinydir" }
